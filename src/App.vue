e163d7f776224675a628666c0746e101820624bc
yautowka <allneb495@gmail.com>
2023-03-19 13:41:04 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Nav, Alert } from '@/components';
+import { useAuthStore } from '@/stores';
+
+const authStore = useAuthStore();
+</script>
+
+<template>
+    <div class="app-container" :class="authStore.user && 'bg-light'">
+        <Nav />
+        <Alert />
+        <div class="container pt-4 pb-4">
+            <router-view />
+        </div>
+    </div>
+</template>
+
+<style>
+@import '@/assets/base.css';
+</style>
